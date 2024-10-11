69af248970bdc05546c79d17178e7d0925eba8e7
AkhilPendlimarry <akhilpendli210@gmail.com>
2024-10-12 01:53:27 +0530

    Modified Footer Data

--- src/HomeComponent/Footer.jsx
+++ src/HomeComponent/Footer.jsx
@@ -3,17 +3,20 @@ import { footMenu, footSocial } from '../data/footerData';
 
 const Footer = () => (
     <footer className="bg-dark text-white pt-5 bg-black">
-        <div className="d-flex flex-wrap justify-content-around text-center mb-4">
-        
-            {servicesData.map(service => (
+        <div className="d-flex flex-column justify-content-center align-items-center text-center mb-4">
+            <h4 className='text-center mb-4'> Our Advantages</h4>
+            <div className='d-flex flex-wrap justify-content-center'>
+             {servicesData.map(service => (
                 <div key={service.id} className="p-3" style={{ flex: "0 1 25%" }}>
-                    <div>{service.icon}</div>
-                    
-                     <h5>{service.title}</h5>
-                     <p>{service.info}</p> 
+                    <span className='text-danger'>{service.icon}</span>
+                    <h5>{service.title}</h5>
+                    <p>{service.info}</p> 
+                   
                     
                 </div>
             ))}
+            </div>
+            
         </div>
         <div className="d-flex flex-wrap justify-content-around text-center">
             {footMenu.map(menu => (
