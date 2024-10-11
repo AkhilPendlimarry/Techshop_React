1bc010751069e60e79a9ecc87b30758b13fb95e6
AkhilPendlimarry <akhilpendli210@gmail.com>
2024-10-12 01:58:43 +0530

    Implemented routing config

--- src/App.js
+++ src/App.js
@@ -3,10 +3,16 @@ import React from 'react';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import LandingPage from "./HomeComponent/LandingPage";
+import AllProductsPage from './ProductsComponent/AllProductsPage';
+import { Routes, Route } from 'react-router-dom';
+
 const App = ()=> {
   return (
     <div>
-      <LandingPage />
+      <Routes>
+        <Route path='/' element={<LandingPage/>}/>
+        <Route path='/ProductsPage' element={<AllProductsPage/>}/>
+      </Routes>
       
     </div>
   );
